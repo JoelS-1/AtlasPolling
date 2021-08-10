c379b26526e528a695d470b90992140e86532cf2
James Campbell <jaymznis@gmail.com>
2021-08-10 18:50:23 -0400

    added Poll and Poll Oprion classes

---
+++ Data/PollOption.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class PollOption
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        public List<string> Votes { get; set; } = new List<string>();
+
+       public int VoteCount
+        {
+            get
+            {
+                int voteCount = Votes.Count;
+                return voteCount;
+            }
+
+        }
+
+        [ForeignKey(nameof(Poll))]
+        public int PollId { get; set; }
+        public virtual Poll Poll { get; set; }
+    }
+}
